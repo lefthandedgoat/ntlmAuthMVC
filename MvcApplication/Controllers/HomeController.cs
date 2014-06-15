9dc6fdbfc17592154d100ad5e93e6418d0f27392
Chris Holt <lefthandedgoat@gmail.com>
2014-06-15 14:48:34 -0500

    initial

---
+++ MvcApplication/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+
+namespace MvcApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize(Users = "chris\\testuser")]
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        [Authorize(Users = "chris\\testuser")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ uitests/Program.fs
@@ -0,0 +1,45 @@
+﻿open canopy
+open runner
+open MikeRogers.NtlmProxy
+open System
+
+let proxy = new NtlmProxy(Uri("http://localhost:7990"), 3999, "chris\\testuser", "1234")
+   
+
+"go to main page" &&& fun _ ->    
+    url "http://localhost:3999"
+    click "About"
+    click "Contact"
+    click "Home"
+
+"do more" &&& fun _ ->
+    click "About"
+    click "Contact"
+    click "Home"
+
+"do more" &&& fun _ ->
+    click "About"
+    click "Contact"
+    click "Home"
+
+"do more" &&& fun _ ->
+    click "About"
+    click "Contact"
+    click "Home"
+
+"do more" &&& fun _ ->
+    click "About"
+    click "Contact"
+    click "Home"
+    
+start chrome
+
+run ()
+
+
+System.Console.ReadKey()
+
+proxy.Dispose()
+quit()
+
+System.Console.ReadKey()
